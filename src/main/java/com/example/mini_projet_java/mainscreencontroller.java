a5441516bf82ca4a9330d40f6585cc3ebdcea07c
AbderrazakBouasker <abderrazakbouasker@gmail.com>
2022-10-09 17:41:01 +0100

    fixed addscene mistake removed useless files of addscene

--- src/main/java/com/example/mini_projet_java/mainscreencontroller.java
+++ src/main/java/com/example/mini_projet_java/mainscreencontroller.java
@@ -9,10 +9,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -20,6 +17,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -58,6 +56,39 @@ public class mainscreencontroller implements Initializable {
 
     @FXML
     private Pane dashboardpanel;
+    @FXML
+    private Pane addmemberpane;
+
+    @FXML
+    private TextField companyinput;
+
+    @FXML
+    private DatePicker enddateinput;
+
+    @FXML
+    private TextField idnumberinput;
+
+    @FXML
+    private TextField lastnameinput;
+
+    @FXML
+    private TextField nameinput;
+
+    @FXML
+    private TextField reductioninput;
+
+    @FXML
+    private Button returnbutton;
+
+    @FXML
+    private DatePicker startdateinput;
+
+    @FXML
+    private Button submitbutton;
+
+    @FXML
+    private Label outlabel;
+
 
     private String dbmidnumber;
     private String dbmname;
@@ -111,7 +142,7 @@ public class mainscreencontroller implements Initializable {
 
     }
 
-    public void dbgetmembersinfo() throws SQLException {
+    /*public void dbgetmembersinfo() throws SQLException {
         Connection connection = DriverManager.getConnection(url,user,password);
         Statement statement =connection.createStatement();
         ResultSet resultSet=statement.executeQuery("select * from members");
@@ -126,7 +157,7 @@ public class mainscreencontroller implements Initializable {
             dbmenddate=resultSet.getString("enddate");
 
         }
-    }
+    }*/
 
 
     public void logout(ActionEvent event) throws IOException {
@@ -137,30 +168,82 @@ public class mainscreencontroller implements Initializable {
         stage.show();
     }
 
-    public void refreshdashboard(ActionEvent event) throws SQLException,IOException {
-        Connection connection = DriverManager.getConnection(url,user,password);
-        Statement statement =connection.createStatement();
-        ResultSet resultSet=statement.executeQuery("select count(idnumber) from members");
+    public void refreshdashboard(ActionEvent event){
+        addmemberpane.setVisible(false);
+        dashboardpanel.setVisible(true);
 
-        while (resultSet.next()) {
-            membersnumber=resultSet.getString("count(idnumber)");
+        try{
+            Connection connection = DriverManager.getConnection(url,user,password);
+            Statement statement =connection.createStatement();
+            ResultSet resultSet=statement.executeQuery("select * from members");
+            while (resultSet.next()) {
+                obser.add(new dashtableimp(resultSet.getString("name"),resultSet.getString("lastname")
+                        ,resultSet.getString("enddate")));
+            }
+            resultSet=statement.executeQuery("select count(idnumber) from members");
 
+            while (resultSet.next()) {
+                membersnumber=resultSet.getString("count(idnumber)");
+
+            }
+            dashboardmemberslabel.setText(membersnumber);
         }
-        dashboardmemberslabel.setText(membersnumber);
+        catch (SQLException e){
+            Logger.getLogger(mainscreencontroller.class.getName()).log(Level.SEVERE,null,e);
+        }
+
+
+        dashboardlistname.setCellValueFactory(new PropertyValueFactory<>("name"));
+        dashboardlistlastname.setCellValueFactory(new PropertyValueFactory<>("lastname"));
+        dashboardlistdays.setCellValueFactory(new PropertyValueFactory<>("remaindays"));
+
+
+
+        dashboardexpirationtable.setItems(obser);
+
 
 
 
     }
 
-     public void addnewscene(ActionEvent event) throws IOException {
-         root = FXMLLoader.load((getClass()).getResource("addnewmember-screen.fxml"));
-         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-         scene = new Scene(root);
-         stage.setScene(scene);
-         stage.show();
 
-     }
+    public void insert(ActionEvent event) throws IOException, SQLException {
+        Connection connection = DriverManager.getConnection(url,user,password);
+        Statement statement =connection.createStatement();
 
+        LocalDate startdate=startdateinput.getValue();
+        LocalDate enddate=enddateinput.getValue();
+        String varcompany=companyinput.getText();
+        String varpayment=reductioninput.getText();
+        String varname=nameinput.getText();
+        String varlastname=lastnameinput.getText();
+        String varidnumber=idnumberinput.getText();
+        //dbgetidnumber(idnumberinput);
+
+        if (nameinput.getText().isEmpty() && lastnameinput.getText().isEmpty() && idnumberinput.getText().isEmpty() && startdate==null && enddate==null){
+            outlabel.setText("Fill all necessary fields");
+        /*} else if (numberid.equals(idnumberinput)) {
+            outlabel.setText("user already exist");*/
+        } else{
+            String varstartdate=startdate.toString();
+            String varenddate=enddate.toString();
+            if(companyinput.getText().isEmpty()){
+                varcompany=null;
+            }
+            if(reductioninput.getText().isEmpty()){
+                varpayment=null;
+            }
+            statement.execute("insert into members values ("+varidnumber+",\""+varname+"\",\""+varlastname+"\",\""+varcompany+"\","+varpayment+",\""+varstartdate+"\",\""+varenddate+"\")");
+            outlabel.setText("");
+        }
+
+    }
+
+    public void switchadd(ActionEvent event){
+        dashboardpanel.setVisible(false);
+        addmemberpane.setVisible(true);
+
+    }
 
 
 
