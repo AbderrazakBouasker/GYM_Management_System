d6de05f9615eb826099ffdcf7e2573b7046acffd
AbderrazakBouasker <abderrazakbouasker@gmail.com>
2022-10-15 22:05:08 +0100

    cleanup

--- src/main/java/dao/Mainscreendao.java
+++ src/main/java/dao/Mainscreendao.java
@@ -9,13 +9,6 @@ import java.sql.*;
 
 public class Mainscreendao {
     Statement statement=null;
-    String idnumber;
-    String name;
-    String lastname;
-    String company;
-    String reduction;
-    String startdate;
-    String enddate;
 
 
 
@@ -41,14 +34,6 @@ public class Mainscreendao {
         ResultSet resultSet=statement.executeQuery("select * from members");
         ObservableList<listtableimp> observ = FXCollections.observableArrayList();
         while (resultSet.next()){
-
-            /*idnumber=resultSet.getString("idnumber");
-            name=resultSet.getString("name");
-            lastname=resultSet.getString("lastname");
-            company=resultSet.getString("companyname");
-            reduction=resultSet.getString("paymentreduction");
-            startdate=resultSet.getString("startdate");
-            enddate=resultSet.getString("enddate");*/
             observ.add(new listtableimp(resultSet.getString("idnumber"), resultSet.getString("name"),resultSet.getString("lastname")
                             , resultSet.getString("companyname"), resultSet.getString("paymentreduction"), resultSet.getString("startdate"), resultSet.getString("enddate")));
 
@@ -56,31 +41,4 @@ public class Mainscreendao {
         return observ;
     }
 
-    public String getIdnumber() {
-        return idnumber;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public String getCompany() {
-        return company;
-    }
-
-    public String getReduction() {
-        return reduction;
-    }
-
-    public String getStartdate() {
-        return startdate;
-    }
-
-    public String getEnddate() {
-        return enddate;
-    }
 }
